591c2456bf32fd228976a1ef3d6a768f986edeb9
chouweihan <chouweihan@gmail.com>
2021-08-07 18:14:41 -0700

    fixed sidebar social links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -38,14 +38,20 @@ const Sidebar = () => {
           <ul>
             {socialLinks.map((link) => {
               return (
-                <Link to={link.url} id={link.id} onClick={closeSidebar}>
+                <a
+                  href={link.url}
+                  target="_blank"
+                  id={link.id}
+                  onClick={closeSidebar}
+                  rel="noreferrer"
+                >
                   <li className="link">
                     <p className="link-text">
                       <span className="fa-fw">{link.icon}</span>
                       {link.name}
                     </p>
                   </li>
-                </Link>
+                </a>
               );
             })}
           </ul>
